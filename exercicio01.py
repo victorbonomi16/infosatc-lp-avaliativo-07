acd6edaaff5cdf3e0a475e881582e4d4058925e9
victorbonomi16 <bononikis@gmail.com>
2020-11-04 17:31:46 -0300

    Feat : Exercicio 01 Concluido

---
+++ exercicio01.py
@@ -0,0 +1,4 @@
+name_age = lambda name, age: print(name, "tem",age,"anos")
+name_typed = str(input("Informe seu nome: "))
+age_typed = int(input("Informe sua idade: "))
+print(name_age(name_typed, age_typed))
