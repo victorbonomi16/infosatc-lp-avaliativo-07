012dba025f8cb91808b6670273f9fb32ecbe7f61
victorbonomi16 <bononikis@gmail.com>
2020-11-09 17:46:06 -0300

    feat:Exercicio 03 Concluido

--- exercicio03.py
+++ exercicio03.py
@@ -1,8 +1,6 @@
-n1 = int(input("Informe o primeiro  número: "))
-n2 = int(input("Informe o segundo   número: "))
-n3 = int(input("Informe o terceiro  número: "))
-n4 = int(input("Informe o quarto    número: "))
-n5 = int(input("Informe o quinto    número: "))
-
-number=filter(lambda x: '>10' in x, [n1,n2,n3,n4,n5])
-print(list(number))
+numeroSalvo=[]
+for x in range(5): 
+    numero=int(input("Informe um numero: "))
+    numeroSalvo.append(numero)
+func = list(filter(lambda x: x >=10, numeroSalvo))
+print(func)
